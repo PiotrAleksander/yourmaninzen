721d098d178578dfb4eadc54da903877f5870fb9
Piotr Aleksander <Piotr Aleksander>
2016-10-23 16:42:29 +0200

    some people will be hating

---
+++ java.js
@@ -0,0 +1,46 @@
+var banner = document.getElementById('banner');
+var paragraph = document.getElementById('paragraphChange');
+function changeText1(){
+  document.getElementById("headerChange").innerHTML = "Ipsum Donor";
+  document.getElementById("paragraphChange").innerHTML = "Changed";
+  
+  banner.style.background = '#333333';
+  paragraph.style.color = #4444444';
+}
+
+function changeText2(){
+  document.getElementById("headerChange").innerHTML = "Ipsum Lorom";
+  document.getElementById("paragraphChange").innerHTML = "Panged";
+  
+  banner.style.background = '#999999';
+  paragraph.style.color = #EAEAEA';
+}
+
+function changeText3(){
+  document.getElementById("headerChange").innerHTML = "Ipsum Boredom";
+  document.getElementById("paragraphChange").innerHTML = "Triangled";
+  
+  banner.style.background = '#298EE7';
+}
+
+function changeText4(){
+  document.getElementById("headerChange").innerHTML = "Ipsum Connom";
+  document.getElementById("paragraphChange").innerHTML = "Tangled";
+  
+  banner.style.background = '#FF6BB0';
+}
+
+function changeText5(){
+  document.getElementById("headerChange").innerHTML = "Ipsum Bockagmon";
+  document.getElementById("paragraphChange").innerHTML = "Mangled";
+  
+  banner.style.background = '#FF7300';
+}
+
+function changeText6(){
+  document.getElementById("headerChange").innerHTML = "Ipsum Lamon";
+  document.getElementById("paragraphChange").innerHTML = "Angled";
+  
+  banner.style.background = '#AE48DA';
+}
+
